826a81ea219a57976f6887a88c623108ec54a507
allaiar <radzhapov.allayar@gmail.com>
2024-07-14 10:53:59 +0600

    first commit

--- src/pages.jsx
+++ src/pages.jsx
@@ -1,14 +1,7 @@
 import React, { useRef } from "react";
 import { useNavigate, Routes, Route } from "react-router-dom";
-import Nav from "./nav";
 import Footer from "./footer";
-import DeliveryInfo from "./deliveryInfo";
-import About from "./about";
-import Privacy from "./privacy";
 import HomePage from "./homePage";
-import PayInfo from "./payInfo";
-import ProductItem from "./productItem";
-import ProductChair from "./productChair";
 import "animate.css";
 
 function Page() {
@@ -36,15 +29,7 @@ function Page() {
 
   return (
     <div className="animate__animated animate__fadeIn">
-      <Nav
-        scrollToComponentChair={scrollToComponentChair}
-        scrollToComponent={scrollToComponent}
-      />
       <Routes>
-        <Route path="/payInfo" element={<PayInfo />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/deliveryInfo" element={<DeliveryInfo />} />
-        <Route path="/about" element={<About />} />
         <Route path="/" element={<HomePage furnitureRef={furnitureRef} />} />
       </Routes>
       <Footer />
